8eedc5336a8c5d9c257fdb22a1c956d90f28a9c6
frederic botella <detection25@gmail.com>
2023-09-12 13:08:51 +0200

    modify readMe.md

--- build/index.js
+++ build/index.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
 const port = process.env.PORT || 8100;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use((0, helmet_1.default)());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
