e5b758146d94972c0408403633a929dcdb6c2c63
frederic botella <detection25@gmail.com>
2023-09-12 13:03:00 +0200

    install hemet and types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 const port = process.env.PORT || 8100;
 const app = express();
 app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
